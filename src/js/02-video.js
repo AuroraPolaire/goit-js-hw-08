a0b5f2d9ce80bd0f0e11e8c5be5ba967a59b5aa8
AuroraPolaire <sea.forest.mountains@gmail.com>
2022-12-29 02:43:03 +0200

    form task finished

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -6,13 +6,13 @@ const player = new Player(iframe);
 
 function onPause(data) {
   const usersTime = data.seconds;
-  localStorage.setItem('feedback-form-state', JSON.stringify(usersTime));
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(usersTime));
 }
 
 player.on('pause', _.throttle(onPause, 1000));
 
 try {
-  const lastUsersTime = localStorage.getItem('feedback-form-state');
+  const lastUsersTime = localStorage.getItem('videoplayer-current-time');
   const parsedLastUsersTime = JSON.parse(lastUsersTime);
 
   player
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import _ from 'lodash';
 
+const form = document.querySelector('.feedback-form');
+const inputValues = {};
+
+function fillFormFields() {
+  try {
+    const data = localStorage.getItem('feedback-form-state');
+    const parsedData = JSON.parse(data);
+
+    if (parsedData === null) {
+      return;
+    }
+
+    for (const item in parsedData) {
+      form.elements[item].value = parsedData[item];
+    }
+  } catch (error) {
+    console.log(parsedData);
+  }
+}
+
+fillFormFields();
+
+function onInputValueGet({ target }) {
+  inputValues[target.name] = target.value;
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(inputValues));
+}
+
+function onSubmitDataSend(event) {
+  event.preventDefault();
+  console.log(inputValues);
+  form.reset();
+}
+
+form.addEventListener('input', _.throttle(onInputValueGet, 500));
+form.addEventListener('submit', onSubmitDataSend);
